9dd9ada24383e42cda2a5977b416de9d97bd0498
Turtle2014 <jfrancis88219@my.npsct.org>
2026-01-10 21:46:35 -0500

    done yet? noooooo...
    Added an array of responses for the askQuestion function.

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function askQuestion(){
   var TheChosenOne = responses[randomIndex];
   document.getElementById("answer").innerHTML = TheChosenOne;
 }
+let responses = ["It is the same in every universe, YES!!!", "There was this random guy and he told me to say yes","Cupcakes have a very good flavor, a mix of sweetness and sprinkles", "I ate it", "The answer Starts with an n and ends with an r and has eve in the middle", "No Doubt contains no which means that it means no, right?"];
+// The random is just a portal to the divine realm and it gives the answer so that you can get the answer, to get the answer requires one button push, somtimes the divines are busy so you might see *walks away*
+
+
+function askQuestion(){
+  let randomIndex = Math.floor(Math.random() * responses.length);
+  var TheChosenOne = responses[randomIndex];
+  document.getElementById("answer").innerHTML = TheChosenOne;
+}
